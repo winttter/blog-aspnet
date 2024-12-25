761bfcd228ede080f46e5457eb320eb43faa394e
Evgeniya Bespalova <tomgen2015@gmail.com>
2024-12-25 22:01:01 +0700

    weather forecast deleted

--- ASP.NET/obj/Debug/net8.0/ASP.NET.AssemblyInfo.cs
+++ ASP.NET/obj/Debug/net8.0/ASP.NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed73d13c3127d5d7d3d4426fe8eadfec0403133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f85cc3c2087639c241929d3dbaecec4afe08c91")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
