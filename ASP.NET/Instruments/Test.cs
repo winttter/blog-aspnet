a29b55fa981c2c858e20dede2509de27ea4abb4d
Evgeniya Bespalova <tomgen2015@gmail.com>
2025-06-17 22:12:08 +0700

    sast trigger

--- ASP.NET/Instruments/Test.cs
+++ ASP.NET/Instruments/Test.cs
@@ -5,6 +5,7 @@
         void Run()
         {
             var password = "mypassword";
+            var password1 = "mypassword";
             string token = "abc123";
             var bytes = Convert.FromBase64String("U29tZVNlY3JldA==");
         }
