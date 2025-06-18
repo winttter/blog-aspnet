a6695ba4075c1bcc441a55f255d1075756e3acf9
Evgeniya Bespalova <tomgen2015@gmail.com>
2025-06-18 11:25:54 +0700

    sast trigger 5

--- ASP.NET/obj/Debug/net8.0/ASP.NET.AssemblyInfo.cs
+++ ASP.NET/obj/Debug/net8.0/ASP.NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a4417449ab7a36012c3ff0382959c02f0d745b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b33d05cba825f24b8bef659d12ebfbb1eadc129")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
