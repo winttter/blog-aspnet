51ebb2317847c8f0a7baf49dedd8b772223fec8b
Evgeniya Bespalova <tomgen2015@gmail.com>
2025-06-19 10:23:48 +0700

    trigger

--- ASP.NET/Instruments/Test.cs
+++ ASP.NET/Instruments/Test.cs
@@ -5,10 +5,10 @@
         void Run()
         {
             var password = "mypassword";
-            var password2 = "1234";
+
             string token = "abc123";
             var bytes = Convert.FromBase64String("U29tZVNlY3JldA==");
-            //  builder.AllowAnyOrigin()
+           
         }
     }
 }
